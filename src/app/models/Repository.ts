bf7bed9e62f62619f071b8c546f2be2da87835b1
José <jose.rubens.veiga@gmail.com>
2021-02-02 18:56:48 -0300

    iniciando a página de detalhes do repositório

--- src/app/models/Repository.ts
+++ src/app/models/Repository.ts
@@ -1,6 +1,7 @@
 export interface Repository {
   full_name: string;
   description: string;
+  issues_url: string;
   owner: {
     avatar_url: string;
     url: string;
--- src/app/repository/repository.component.ts
+++ src/app/repository/repository.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {
+  faChevronLeft,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
+import { Repository } from '../models/Repository';
+import { GithubService } from '../services/github.service';
+
+interface RepositoryParams {
+  owner: string;
+  repositoryName: string;
+}
 
 @Component({
   selector: 'app-repository',
   templateUrl: './repository.component.html',
-  styleUrls: ['./repository.component.scss']
+  styleUrls: ['./repository.component.scss'],
 })
 export class RepositoryComponent implements OnInit {
+  faChevronLeft = faChevronLeft;
+  faChevronRight = faChevronRight;
+  repository: Repository;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private githubService: GithubService
+  ) {
+    this.loadParams();
   }
 
+  ngOnInit(): void {}
+
+  private async loadParams(): Promise<void> {
+    this.activatedRoute.params.subscribe((params) => {
+      const { owner, repositoryName } = params as RepositoryParams;
+
+      this.loadRepository(`${owner}/${repositoryName}`);
+    });
+  }
+
+  private loadRepository(repositoryName: string): void {
+    this.githubService.getRepository(repositoryName).subscribe((repository) => {
+      this.repository = repository;
+    });
+  }
 }
